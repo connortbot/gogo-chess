bd3940368ab38c1650dfb032ff4c8b95de2c0982
connortbot <loiconnor8@gmail.com>
2023-04-02 09:17:16 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -4,6 +4,37 @@ const { Collection, Events, ActionRowBuilder, SelectMenuBuilder, Embed, EmbedBui
 const { NormalGoGos, Weapons, Gear, Dungeons, SellValues } = require('../balance.json');
 const battles = require('../battles');
 
+async function execute(interaction) {
+    await interaction.deferReply();
+    const rolls = interaction.options.getInteger('rolls');
+    const usr = await database.getUser(interaction.user.id.toString());
+    for (let i=0; i<rolls; i++) {
+        var r = Math.random() * (Object.keys(NormalGoGos).length - 1);
+        r = Math.round(r);
+        const possibleGoGo = Object.keys(NormalGoGos)[r];
+        if (Math.random() <= 0.1) {
+            const inv = usr.inventory.split("-");
+            let valid = true;
+            for (let j=0; j<inv.length; j++) {
+                if (possibleGoGo == inv[j].split('#')[0]) {
+                    await interaction.channel.send(":sob: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                const addedID = await database.createNewGoGo(possibleGoGo);
+                await usr.update({inventory: inv+"-"+addedID});
+                await interaction.channel.send(":star: You rolled a "+possibleGoGo+"! :star: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
+            }
+        } else {
+            await interaction.channel.send(":broken_heart: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
+        }
+    }
+    await interaction.editReply("Best of luck!");
+}
+
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,29 +45,5 @@ module.exports = {
                 .setName('rolls')
                 .setDescription("The number of rolls you wish to use.")
                 .setRequired(true)),
-    async execute(interaction) {
-        await interaction.deferReply();
-        const rolls = interaction.options.getInteger('rolls');
-        const usr = await database.getUser(interaction.user.id.toString());
-        for (let i=0; i<rolls; i++) {
-            var r = Math.random() * Object.keys(NormalGoGos).length;
-            r = Math.round(r);
-            const possibleGoGo = Object.keys(NormalGoGos)[r];
-            if (Math.random() <= 0.1) {
-                const addedID = await database.createNewGoGo(possibleGoGo);
-                const inv = usr.inventory.split("-");
-                for (let j=0; j<inv.length; j++) {
-                    if (possibleGoGo == inv[j].split('#')[0]) {
-                        await interaction.channel.send(":sob: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
-                        break;
-                    }
-                }
-                await usr.update({inventory: inv+"-"+addedID});
-                await interaction.channel.send(":star: You rolled a "+possibleGoGo+"! :star: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
-            } else {
-                await interaction.channel.send(":broken_heart: (roll "+(i+1).toString()+"/"+rolls.toString()+")");
-            }
-        }
-        await interaction.editReply("Best of luck!");
-    }
+    execute
 }
