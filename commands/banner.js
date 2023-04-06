298287d5ecae88ebbbb4a020c728687c84e34b2c
jj9kim <kimjoshua355@gmail.com>
2023-04-05 21:46:12 -0400

    Banner add

--- commands/banner.js
+++ commands/banner.js
@@ -15,4 +15,4 @@ const exampleEmbed = new EmbedBuilder()
 	.setImage('https://i.imgur.com/AfFp7pu.png')
 	.setTimestamp()
 	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    return EmbedBuilder;
+    channel.send({ embeds: [exampleEmbed] });
