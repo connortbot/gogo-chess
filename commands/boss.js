73b2bd11d878041208d99ad6cecfc59dffec2060
connortbot <loiconnor8@gmail.com>
2023-06-21 18:08:24 -0400

    Remove buggy co-op from dungeons and boss

--- commands/boss.js
+++ commands/boss.js
@@ -8,11 +8,7 @@ const battles = require('../battles');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('boss')
-        .setDescription("Challenge K'aeda, the Dragon of Crystals.")
-        .addMentionableOption((option) =>
-            option
-                .setName('teammateone')
-                .setDescription('Ping your teammate!')),
+        .setDescription("Challenge K'aeda, the Dragon of Crystals."),
     async execute(interaction) {
         const user = await database.getUser(interaction.user.id.toString());
         const limits = user.fight_limits.split('-');
@@ -25,7 +21,6 @@ module.exports = {
             return;
         }
         await interaction.deferReply();
-        const coop = interaction.options.getMentionable('teammateone');
         if (user == null) {
             await interaction.editReply("You can't fight without joining GoGo World! Use **/register** to join today!")
             return
@@ -34,75 +29,23 @@ module.exports = {
             await interaction.editReply('Please form your team first.');
             return
         }
-        if (coop == null) {
-            for (let i=0; i<1; i++) {
-                var b = await battles.start_battle(user.team.split('-'),["Monster/Dragon K'aeda"],interaction.channel,[user]);
-                console.log(b);
-                if (b != "side1") {
-                    // Failed the Fight
-                    await interaction.editReply('You failed the fight.');
-                    return
-                }
-                if (b == "noTeam") {
-                    await interaction.editReply('You did not select a team. (Use **/team** to set one up)')
-                    return
-                }
+        for (let i=0; i<1; i++) {
+            var b = await battles.start_battle(user.team.split('-'),["Monster/Dragon K'aeda"],interaction.channel,[user]);
+            console.log(b);
+            if (b != "side1") {
+                // Failed the Fight
+                await interaction.editReply('You failed the fight.');
+                return
             }
-            // Defeated the Boss
-            await interaction.channel.send('There is no loot currently available for this boss.');
-            user.fight_limits = `1-${limits[1]}-${limits[2]}-${limits[3]}-${limits[4]}`;
-            await user.save();
-            await interaction.editReply(interaction.user.username+' defeated the boss!');
-        } else {
-            const coopUser = await database.getUser(coop.user.id.toString());
-            if ((coopUser.team.split('-').length == 0)) {
-                await interaction.editReply('<@'+coop.id.toString()+">, please form your team.");
+            if (b == "noTeam") {
+                await interaction.editReply('You did not select a team. (Use **/team** to set one up)')
+                return
             }
-            await interaction.channel.send('<@'+coop.id.toString()+">, would you like to join <@"+interaction.user.id.toString()+"> to conquer this dungeon? (type Y)");
-            const filter = response => (response.author.id.toString() === coop.id.toString() && response.author.id.toString() != coop.id.toString());
-            const collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-                .then(async collected => {
-                    if (collected.first().content == 'Y') {
-                        var team = [];
-                        var uTeam = user.team.split('-');
-                        var cTeam = coopUser.team.split('-');
-                        for (let i=1; i<(coopUser.team.split('-').length+user.team.split('-').length); i++) {
-                            if (i % 2 == 0) { //even
-                                if (cTeam.length == 0) {
-                                    continue;
-                                }
-                                team.push(cTeam[0]);
-                                cTeam.shift();
-                            } else {
-                                if (uTeam.length == 0) {
-                                    continue;
-                                }
-                                team.push(uTeam[0]);
-                                uTeam.shift();
-                            }
-                        }
-                        for (let i=0; i<1; i++) {
-                            var b = await battles.start_battle(team,["Monster/Dragon K'aeda"],interaction.channel,[user,coopUser]);
-                            if (b != "side1") {
-                                // Failed the Fight
-                                interaction.editReply('You failed the fight.');
-                            }
-                        }
-                        // Defeated the Dungeon
-                        await interaction.channel.send('There is no loot currently available for this boss.');
-                        user.fight_limits = `1-${limits[1]}-${limits[2]}-${limits[3]}-${limits[4]}`;
-                        await user.save();
-                        coopUser.fight_limits = `1-${limits[1]}-${limits[2]}-${limits[3]}-${limits[4]}`;
-                        await coopUser.save();
-                        await interaction.editReply(interaction.user.username+"slayed K'aeda with "+coop.username+".");
-                    } else {
-                        interaction.editReply('<@'+coop.id.toString()+"> doesn't want to fight. Until next time!");
-                    }
-                })
-                .catch(collected => {
-                    // No response.
-                    interaction.editReply('<@'+coop.id.toString()+"> doesn't want to fight. Until next time!");
-                })
         }
+        // Defeated the Boss
+        await interaction.channel.send('There is no loot currently available for this boss.');
+        user.fight_limits = `1-${limits[1]}-${limits[2]}-${limits[3]}-${limits[4]}`;
+        await user.save();
+        await interaction.editReply(interaction.user.username+' defeated the boss!');
     }
 }
--- commands/dungeon.js
+++ commands/dungeon.js
@@ -29,11 +29,7 @@ module.exports = {
             option
                 .setName('number')
                 .setDescription("The corresponding number of the dungeon. Try !dungeons if you don't know which one!")
-                .setRequired(true))
-        .addMentionableOption((option) =>
-            option
-                .setName('teammateone')
-                .setDescription('Ping your teammate!'),),
+                .setRequired(true)),
     async execute(interaction) {
         const date = new Date();
         const curr_date = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`
@@ -49,83 +45,28 @@ module.exports = {
         }
         await interaction.deferReply();
         const dungeonName = Object.keys(Dungeons)[interaction.options.getInteger('number')-1];
-        const coop = interaction.options.getMentionable('teammateone');
         if (user.team.split('-').length == 0) {
             await interaction.editReply('Please form your team first.');
         }
-        if (coop == null) {
-            for (let i=0; i<Dungeons[dungeonName]["waves"].length; i++) {
-                var b = await battles.start_battle(user.team.split('-'),Dungeons[dungeonName]["waves"][i],interaction.channel,[user]);
-                if (b != "side1") {
-                    // Failed the Fight
-                    await updateLimits([user]);
-                    await interaction.editReply('You failed the fight');
-                    return;
-                }
-                if (b == "noTeam") {
-                    await interaction.editReply('You did not select a team. (Use **/team** to set one up)')
-                    return
-                }
+        for (let i=0; i<Dungeons[dungeonName]["waves"].length; i++) {
+            var b = await battles.start_battle(user.team.split('-'),Dungeons[dungeonName]["waves"][i],interaction.channel,[user]);
+            if (b != "side1") {
+                // Failed the Fight
+                await updateLimits([user]);
+                await interaction.editReply('You failed the fight');
+                return;
             }
-            // Defeated the Dungeon
-            await updateLimits([user]);
-            await interaction.channel.send('You were awarded the '+Gear[Dungeons[dungeonName]["loot"]]["name"]+' and '+Dungeons[dungeonName]["bones"].toString()+' bones upon clearing this dungeon.');
-            const gearID = await database.createNewGear(Dungeons[dungeonName]["loot"]);
-            await database.giveGearWeapon(interaction.user.id.toString(),gearID);
-            await database.giveBones(interaction.user.id.toString(),Dungeons[dungeonName]["bones"]);
-            await interaction.editReply(interaction.user.username+' cleared the '+dungeonName+'.');
-        } else {
-            const coopUser = await database.getUser(coop.user.id.toString());
-            if (coopUser.team.split('-').length == 0) {
-                await interaction.editReply('<@'+coop.id.toString()+">, please form your team.");
+            if (b == "noTeam") {
+                await interaction.editReply('You did not select a team. (Use **/team** to set one up)')
+                return
             }
-            await interaction.channel.send('<@'+coop.id.toString()+">, would you like to join <@"+interaction.user.id.toString()+"> to conquer this dungeon? (type Y)");
-            const filter = response => (response.author.id.toString() === coop.id.toString() && response.author.id.toString() != coop.id.toString());
-            const collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-                .then(async collected => {
-                    if (collected.first().content == 'Y') {
-                        for (let i=0; i<Dungeons[dungeonName]["waves"].length; i++) {
-                            var team = [];
-                            var uTeam = user.team.split('-');
-                            var cTeam = user.team.split('-');
-                            for (let i=1; i<(coopUser.team.split('-').length+user.team.split('-').length); i++) {
-                                if (i % 2 == 0) { //even
-                                    if (cTeam.length == 0) {
-                                        continue;
-                                    }
-                                    team.push(cTeam[0]);
-                                    cTeam.shift();
-                                } else {
-                                    if (uTeam.length == 0) {
-                                        continue;
-                                    }
-                                    team.push(uTeam[0]);
-                                    uTeam.shift();
-                                }
-                            }
-                            var b = await battles.start_battle(team,Dungeons[dungeonName]["waves"][i],interaction.channel,[user,coopUser]);
-                            if (b != "side1") {
-                                // Failed the Fight
-                                await updateLimits([user,coopUser]);
-                                await interaction.editReply('You failed the fight');
-                                return;
-                            }
-                        }
-                        // Defeated the Dungeon
-                        await updateLimits([user,coopUser]);
-                        await interaction.channel.send('You were awarded the '+Gear[Dungeons[dungeonName]["loot"]]["name"]+' and '+Dungeons[dungeonName]["bones"].toString()+' bones upon clearing this dungeon.');
-                        const gearID = await database.createNewGear(Dungeon[dungeonName]["loot"]);
-                        await database.giveGearWeapon(interaction.user.id.toString(),gearID);
-                        await database.giveBones(interaction.user.id.toString(),Dungeons[dungeonName]["bones"]);
-                        await interaction.editReply(interaction.user.username+' cleared the '+dungeonName+'.');
-                    } else {
-                        interaction.editReply('<@'+coop.id.toString()+"> doesn't want to fight. Until next time!");
-                    }
-                })
-                .catch(collected => {
-                    // No response.
-                    interaction.editReply('<@'+coop.id.toString()+"> doesn't want to fight. Until next time!");
-                })
         }
+        // Defeated the Dungeon
+        await updateLimits([user]);
+        await interaction.channel.send('You were awarded the '+Gear[Dungeons[dungeonName]["loot"]]["name"]+' and '+Dungeons[dungeonName]["bones"].toString()+' bones upon clearing this dungeon.');
+        const gearID = await database.createNewGear(Dungeons[dungeonName]["loot"]);
+        await database.giveGearWeapon(interaction.user.id.toString(),gearID);
+        await database.giveBones(interaction.user.id.toString(),Dungeons[dungeonName]["bones"]);
+        await interaction.editReply(interaction.user.username+' cleared the '+dungeonName+'.');
     }
 }
